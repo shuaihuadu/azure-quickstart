49ad2fbb31ad5e6b3a815cabc0f2db3e07860357
ShuaiHua Du <shuaihua.du@outlook.com>
2024-06-25 11:36:11 +0800

    Fix build errors.

--- examples/AzureAIServiceExamples/GlobalUsings.cs
+++ examples/AzureAIServiceExamples/GlobalUsings.cs
@@ -2,8 +2,6 @@
 global using Azure.AI.Vision.ImageAnalysis;
 global using Azure.QuickStart.Shared;
 global using Azure.QuickStart.Shared.Resources;
-global using Microsoft.CognitiveServices.Speech;
-global using Microsoft.CognitiveServices.Speech.Audio;
 global using System.Net.Http.Headers;
 global using System.Numerics.Tensors;
 global using System.Text;
